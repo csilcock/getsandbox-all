1b74ece6126e52bccd6482c7fa31752587d8812b
Sandbox Sync <support@getsandbox.com>
2015-12-09 14:46:06 -0500

    Changes

--- ToolAccess/tool-access-data.js
+++ ToolAccess/tool-access-data.js
@@ -22,13 +22,13 @@ var generate = function generate(){
 
 var generateOrgUnit = function generateOrgUnit(orgUnitId){
 
-    var allData = {};
+    var allData = {orgUnitId: orgUnitId};
     for(var i =0; i<consts.ROLE_IDS.length; i++){
         var role = consts.ROLE_IDS[i];
-        allData[role.id] = {};
+        allData[orgUnitId][role.id] = {};
         for (var j = 0; j<role.tools.length; j++){
             var tool = role.tools[j];
-            allData[role.id][tool.id] = {};
+            allData[orgUnitId][role.id][tool.id] = {};
             var date = Date.now();
             for(var k = date - consts.MS_PER_YEAR; k <= date; k += consts.MS_PER_DAY){
                 allData[orgUnitId][role.id][tool.id][k] = Math.floor((Math.random() * 100 * tool.useLevel * role.useLevel) + 1).toString();
