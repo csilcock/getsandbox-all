4163e2b08027f646f03a35a554bfe4d45f17faf7
Sandbox Sync <support@getsandbox.com>
2015-12-09 13:50:11 -0500

    Changes

--- main.js
+++ main.js
@@ -16,7 +16,9 @@ Sandbox.define('/d2l/api/adp/unstable/aggregatedEvents/{id}','GET', function(req
 });
 
 Sandbox.define('/d2l/api/adp/unstable/aggregatedEvents/{id}/{courseId}/Course Offering','GET', function(req, res) {
-    var result = generateCourseAccessData(req.params.id, req.params.courseId);
+    var dates = utils.parseDates(req.query.startTime, req.query.endTime);
+    
+    var result = generateCourseAccessData(req.params.id, req.params.courseId, dates.startTime, dates.endTime);
 
     res.type('application/json');
     res.status(200);
