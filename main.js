83dd7a5ec27d4ec1af6a281bb83db1cca09fc573
Sandbox Sync <support@getsandbox.com>
2015-12-09 14:39:41 -0500

    Changes

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var generateCourseAccessData = require('CourseAccess/course-access-data.js').generate,
     generateToolAccessData = require('ToolAccess/tool-access-data.js').generate,
+    generateToolAccessDataOrgUnit = require('ToolAccess/tool-access-data.js').generateOrgUnit,
     generateTotalLoginsData = require('TotalLogins/total-logins-data.js').generate,
     utils = require('Common/utils.js');
 
@@ -23,7 +24,7 @@ Sandbox.define('/d2l/api/adp/unstable/aggregatedEvents/{id}/{courseId}/Course Of
 });
 
 Sandbox.define('/d2l/api/adp/unstable/aggregatedEvents/{id}/{courseId}','GET', function(req, res) {
-    var result = generateToolAccessData(req.params.courseId, undefined, dates.startTime, dates.endTime);
+    var result = generateToolAccessDataOrgUnit(req.params.courseId);
 
     res.type('application/json');
     res.status(200);
